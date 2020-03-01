36c7f0ab21440b03079c0bb2286ec0648f9d15b6
Benny Ho <benny.ho2530@gmail.com>
2020-03-01 16:27:58 +0100

    added btn function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 
+(function() {
+
+    const btn = document.querySelector('#btn');
+
+    const array = ['yellow', 'red', 'blue', 'green', 'purple', 'orange', 'rose']
+
+    btn.addEventListener('click', function() {
+
+        let random = Math.floor(Math.random() * array.length);
+        
+        document.body.style.backgroundColor = array[random]
+    })
+
+})();
